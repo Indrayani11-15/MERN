e21f7f2c3dcb2e890a83b9ca2076071837c63695
Indrayani Verulkar <118429007+Indrayani11-15@users.noreply.github.com>
2025-04-10 19:29:56 +0530

    week2

--- Week2/que4.js
+++ Week2/que4.js
@@ -6,12 +6,3 @@ for (let i = 1; i <= 5; i++) {
 
 console.log("Numbers from 1 to 5:", numArray);
 
-// 5: Function to return square of a number
-function square(number) {
-    return number * number;
-}
-
-let input = 6;
-let squared = square(input);
-
-console.log("Square of", input, "is", squared);
---
+++ Week2/que5.js
@@ -0,0 +1,8 @@
+function square(number) {
+    return number * number;
+}
+
+let input = 6;
+let squared = square(input);
+
+console.log("Square of", input, "is", squared);
